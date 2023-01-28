c26b1e198ff80ac3e0ca91354641eb56ba94499f
vinay81475689 <112241441+vinay81475689@users.noreply.github.com>
2023-01-28 13:30:52 +0530

    Add files via upload

---
+++ odd and even.c
@@ -0,0 +1,29 @@
+int main()
+{
+    int arr[100],i,num;
+    printf("Enter size of the array \n");
+    scanf("%d",&num);
+    printf("Enter the elements of the array\n");
+    i=0;
+    while(i<num){
+        scanf("%d",&arr[i]);
+         i++;
+    }
+    printf("\nEven numbers of the array are \n");
+    i=0;
+     while(i<num){
+        if(arr[i]%2==0){
+            printf("%d \t",arr[i]);
+        }
+        i++;
+    }
+    printf("\nOdd numbers of the array are \n");
+    i=0;
+     while(i<=num){
+        if (arr[i]%2==1){
+            printf("%d \t",arr[i]);
+        }
+        i++;
+    }
+    return 0;
+}
